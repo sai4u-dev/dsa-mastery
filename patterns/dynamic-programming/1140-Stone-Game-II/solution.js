f3cb7d3e9cc9676450ddbccf0a8a208cbb77fc5d
Sai Narendra <tannirusainarendra81@gmail.com>
2026-08-09 19:36:46 +0530

    Added README and solution for 1140. Stone Game II problem with dynamic programming approach

---
+++ patterns/dynamic-programming/1140-Stone-Game-II/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} piles
+ * @return {number}
+ */
+var stoneGameII = function (piles) {
+    const n = piles.length;
+    const suf = Array(n + 1).fill(0);
+    const dp = Array.from({ length: n }, () => Array(n + 1).fill(0));
+
+    for (let i = n - 1; i >= 0; i--) suf[i] = suf[i + 1] + piles[i];
+    for (let i = n - 1; i >= 0; i--) {
+        for (let m = 1; m <= n; m++) {
+            if (2 * m >= n - i) {
+                dp[i][m] = suf[i];
+                continue;
+            }
+
+            for (let x = 1; x <= 2 * m; x++) {
+                dp[i][m] = Math.max(
+                    dp[i][m],
+                    suf[i] - dp[i + x][Math.max(m, x)]
+                );
+            }
+        }
+    }
+
+    return dp[0][1];
+};
